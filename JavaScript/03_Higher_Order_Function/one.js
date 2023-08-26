d94b9723785a69d3db942e9832582abfc40e2bfb
unknown <deepgarg157@gmail.com>
2023-08-26 20:21:33 +0530

    High order function

--- JavaScript/03_Higher_Order_Function/one.js
+++ JavaScript/03_Higher_Order_Function/one.js
@@ -6,7 +6,7 @@ const newArray=myArray.reduce((acc, currval) => acc*currval,1)
 
 // console.log(newArray);
  
-
+// calculator
 function sum(x, y){
     return x+y
 }
@@ -28,10 +28,22 @@ function calculateFunction(fn, x, y){
     return fn(x, y)
 }
 
-console.log('sum >', calculateFunction(sum, 2, 2));
+// console.log('sum >', calculateFunction(sum, 2, 2));
 
-console.log('subtract >', calculateFunction(subract, 2, 2));
+// console.log('subtract >', calculateFunction(subract, 2, 2));
 
-console.log('multiple >', calculateFunction(multiple, 2, 2));
+// console.log('multiple >', calculateFunction(multiple, 2, 2));
 
-console.log('divide >', calculateFunction(divide, 2, 2));
+// console.log('divide >', calculateFunction(divide, 2, 2));
+
+
+function deliveryProduct(name, callback){
+    console.log('done the delivery of the', name );
+    callback()
+}
+
+function callBack(){
+    console.log('Successfully delivered the product');
+}
+
+deliveryProduct('HP laptop', callBack)
