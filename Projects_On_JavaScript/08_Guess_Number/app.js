4775148f148efbd0021221b10d84ae931a787b6e
unknown <deepgarg157@gmail.com>
2023-08-29 12:33:30 +0530

    guess the number

--- Projects_On_JavaScript/08_Guess_Number/app.js
+++ Projects_On_JavaScript/08_Guess_Number/app.js
@@ -1,1 +1,48 @@
-const randomNumber=(parseInt(Math.random()*100+1))
+const randomNumber = (parseInt(Math.random() * 100 + 1))
+
+const submitGuess = document.querySelector('#subt')
+const inputUser = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const lowOrHi = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas')
+
+let previousGuess = []
+
+let newGuess = 1;
+
+let playGame = true;
+
+if (playGame) {
+    submitGuess.addEventListener('click', function(e){
+        e.preventDefault
+        const guess=parseInt(inputUser.value)
+        console.log(guess);
+        validateGuess(guess)
+    })
+}
+
+function validateGuess(guess) {
+    // 
+}
+
+function checkGuess(guess) {
+    // 
+}
+
+function displayguess(guess) {
+    // 
+}
+
+function displayMessage(message) {
+    // 
+}
+
+function endGame() {
+    // 
+}
+
+function startGame() {
+    // 
+}
+
