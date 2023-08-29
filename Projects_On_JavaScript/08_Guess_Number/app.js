15722be02e5a34cf9c168da121b578cce535ff9d
unknown <deepgarg157@gmail.com>
2023-08-29 13:25:34 +0530

    guess the number

--- Projects_On_JavaScript/08_Guess_Number/app.js
+++ Projects_On_JavaScript/08_Guess_Number/app.js
@@ -23,11 +23,33 @@ if (playGame) {
 }
 
 function validateGuess(guess) {
-    // 
+    if(isNaN(guess)){
+       alert('PLease enter the valid number')
+    }else if(guess < 1){
+        alert('enter the number more than 1')
+    }else if(guess > 100){
+        alert('enter the number less than 100')
+    }else{
+        previousGuess.push(guess)
+        if(newGuess === 11){
+            displayguess(guess)
+            displayMessage('game over. random number was ' `${randomNumber}`)
+            endGame()
+        }else{
+            displayguess(guess)
+            checkGuess(guess)
+        }
+    }
 }
 
 function checkGuess(guess) {
-    // 
+    if(guess===randomNumber){
+        displayMessage('you guess the right number')
+    }else if(guess < randomNumber){
+        displayMessage('the number is TOOO low')
+    }else if(guess > randomNumber){
+        displayMessage('the number is TOOO high')
+    }
 }
 
 function displayguess(guess) {
