fa87f8406ee96771494d1ad64f8823be840ba9c8
unknown <deepgarg157@gmail.com>
2023-09-01 00:37:07 +0530

    update calculator

--- Projects_On_JavaScript/09_Calculator/app.js
+++ Projects_On_JavaScript/09_Calculator/app.js
@@ -1,20 +1,25 @@
 // Todo: Make M+ M- and MC functional
 let string = "";
 let buttons = document.querySelectorAll('.btn');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
+let inputs = document.querySelector('input')
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
       string = eval(string);
-      document.querySelector('input').value = string;
+      inputs.value = string;
     }
-    else if(e.target.innerHTML == 'AC'){
+    else if (e.target.innerHTML == 'AC') {
       string = ''
-      document.querySelector('input').value = string;
+      inputs.value = string;
     }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
+    else if (e.target.innerHTML == 'DEL') {
+      string = string.substring(0, string.length - 1)
+      inputs.value = string
+    }
+    else {
+      console.log(e.target)
+      string = string + e.target.innerHTML;
+      inputs.value = string;
+    }
   })
 })
