0de8be1c747962f3e42a3ecc7ec95b9204f6a286
unknown <deepgarg157@gmail.com>
2023-09-02 16:55:31 +0530

    promises

--- JavaScript/06_Advance_JS/promises.js
+++ JavaScript/06_Advance_JS/promises.js
@@ -9,11 +9,25 @@
 //      console.log('promises is resolved');
 // })
 
-new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        console.log('async is consumed');
-        resolve()
-    }, 1000)
-}).then(function () {
-    console.log('promises is resolved');
+// new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//         console.log('async is consumed');
+//         resolve()
+//     }, 1000)
+// }).then(function () {
+//     console.log('promises is resolved');
+// })
+
+const promiseThree= new Promise(function(resolve, reject){
+    setTimeout(function(){
+         resolve({
+            name:'Deepanshu',
+            age:26,
+            email:'deep56@gmail.com'
+         })
+    },1000)
+})
+
+promiseThree.then(function(user){
+console.log(user);
 })
