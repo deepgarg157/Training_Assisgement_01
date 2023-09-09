842bbeb602e69b44817c52be73397f34c7de8942
unknown <deepgarg157@gmail.com>
2023-09-09 23:34:46 +0530

    closure

--- JavaScript/08_Classes_oops/Getter_Setter.js
+++ JavaScript/08_Classes_oops/Getter_Setter.js
@@ -3,7 +3,25 @@ class user{
         this.username=username
         this.password=password
     }
+
+    get username(){
+        return this._username
+    }
+
+    set username(value){
+        this._username=value
+    }
+
+    get password(){
+        return this._password.toUpperCase()
+    }
+
+    set password(value){
+        this._password=value.toUpperCase()
+    }
 }
 
-const newUser= new user('Deep', 1234)
+const newUser= new user('Deep', 'abc')
 console.log(newUser.password);
+console.log(newUser.username);
+
