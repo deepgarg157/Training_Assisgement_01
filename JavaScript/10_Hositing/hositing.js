f89d1118f3a4256c36611effe6db9eab4be22625
unknown <deepgarg157@gmail.com>
2023-09-12 10:30:29 +0530

    update

---
+++ JavaScript/10_Hositing/hositing.js
@@ -0,0 +1,9 @@
+// var x=7;
+// function getValue(){
+//     console.log('hello world');
+// }
+
+// getValue();
+// console.log(x);
+
+console.log(x);
--- JavaScript/Practice/javaScript.js
+++ JavaScript/Practice/javaScript.js
@@ -1,26 +1,18 @@
-let string = ""
-let buttons = document.querySelectorAll('.btn')
-let displayValue = document.getElementById('display')
+let date=document.querySelector('button')
+let dateTime=new Date()
+console.log(dateTime);
 
-Array.from(buttons).forEach((button) => {
-    button.addEventListener('click', function (e) {
-        if (e.target.innerHTML == '=') {
-            string = eval(string)
-            displayValue.value = string
-        }
+let para=document.getElementById('demo')
 
-        else if (e.target.innerHTML == 'RES') {
-            string = ""
-            displayValue.value = string
-        } else if (e.target.innerHTML == 'DEL') {
-            string = string.substring(0, string.length - 1)
-            displayValue.value = string
-        }
-        else {
-            string = string + e.target.innerHTML
-            displayValue.value = string
-            console.log(e.target);
 
-        }
+
+
+function showdate(){
+    date.addEventListener('click', function(){
+        para.innerHTML=dateTime
     })
-})  
+}
+
+
+
+setInterval(showdate(),1000)
