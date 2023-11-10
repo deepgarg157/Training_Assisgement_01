b7b7955b2e2de93f2f059f497acf3b762be57ad5
unknown <deepgarg157@gmail.com>
2023-11-10 13:55:26 +0530

    spread and rest operator

--- JavaScript/12_spread_rest_opeartor/spread_rest_opeartor.js
+++ JavaScript/12_spread_rest_opeartor/spread_rest_opeartor.js
@@ -18,3 +18,62 @@ let obj2={
 }
 
 console.log(obj2);
+
+
+// rest example in java script
+
+function sumAdd(a,b,c,...other){
+    console.log(other)
+    console.log(other[0])
+    console.log(arguments)
+    return a+b+c;
+}
+
+const result = sumAdd(1,2,3,4,5,6);
+console.log(result)
+
+// spread operator in java script
+
+const arr =["ankit", "deep", "akshay"]
+
+function spread(name1, name2, name3){
+  console.log(name1, name2, name3)
+}
+
+spread(arr[0], arr[1], arr[2])
+spread(...arr) // best to use ES6 in the java script......
+spread(arr)
+
+
+// object ka use in rest 
+
+const info = {
+    name : "Deepanshu",
+    age : 26,
+    email : ["deep@123" , "ankit@123"]
+}
+
+info.city = "budhana"
+
+console.log(info.name)
+console.log(info['name'])
+console.log(info.city)
+
+const {age,...rest} = info; // D structure
+
+console.log(age, rest)
+
+// object ka use => spread
+
+const userInfo = {
+    name : "Deep",
+    age : 27,
+    email :"Deepanshu@12345"
+}
+
+const userInfo1 = {
+    ...userInfo,
+    number : 123
+}
+
+console.log(userInfo1)
