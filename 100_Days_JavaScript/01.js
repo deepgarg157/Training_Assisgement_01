d4f3c0fc324c3dcb86527d68cd17157e033245ee
Deepanshu Garg <139165184+deepgarg157@users.noreply.github.com>
2024-05-30 21:44:25 +0530

    update 100 days

--- 100_Days_JavaScript/01.js
+++ 100_Days_JavaScript/01.js
@@ -1,5 +1,6 @@
 // a question in which you have to find out the greatest string by creating a function find_Largest_String. If there are mulitple largest strings , it will encounter the very first one.
 
+// sort method to find out the result
 function find_Largest_String(str) {
     if (str.trim().length === 0) {
         return false
@@ -13,3 +14,17 @@ function find_Largest_String(str) {
 
 const res = find_Largest_String('My name is Deepanshu Garg')
 console.log(res); 
+
+
+// reduce method to find out the result
+function find_Largest_String_reduce(str) {
+    if (str.trim().length === 0) {
+        return false
+    }
+
+    let words = str.split(' ')
+    return words.reduce((acc, curr) => curr.length > acc.length ? curr : acc ,'')
+}
+
+const res1 = find_Largest_String_reduce('My name is Deepanshu Garg')
+console.log(res1); 
